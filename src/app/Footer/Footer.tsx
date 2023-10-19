72b47e39812a2c023ab4c9b37ea09acd2a344ffd
ben <ben21catton@gmail.com>
2023-10-19 21:16:57 +0100

    first commit

---
+++ src/app/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+export default function Footer() {
+  return (
+    <footer className="bg-neutral p-10 text-neutral-content">
+      <div className="footer m-auto max-w-7xl">
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link-hover link">Branding</a>
+          <a className="link-hover link">Design</a>
+          <a className="link-hover link">Marketing</a>
+          <a className="link-hover link">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link-hover link">Branding</a>
+          <a className="link-hover link">Design</a>
+          <a className="link-hover link">Marketing</a>
+          <a className="link-hover link">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link-hover link">Branding</a>
+          <a className="link-hover link">Design</a>
+          <a className="link-hover link">Marketing</a>
+          <a className="link-hover link">Advertisement</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/app/products/[id]/AddToCartButton.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState, useTransition } from "react";
+
+interface AddToCartButtonProps {
+  productId: string;
+  incrementProductQuantity: (productId: string) => Promise<void>;
+}
+
+export default function AddToCartButton({
+  productId,
+  incrementProductQuantity,
+}: AddToCartButtonProps) {
+  const [isPending, startTransition] = useTransition();
+  const [success, setSuccess] = useState(false);
+
+  return (
+    <div className="flex items-center gap-2">
+      <button
+        className="btn-primary btn"
+        onClick={() => {
+          setSuccess(false);
+          startTransition(async () => {
+            await incrementProductQuantity(productId);
+            setSuccess(true);
+          });
+        }}
+      >
+        Add to Cart
+      </button>
+      {isPending && <span className="loading loading-dots loading-md" />}
+      {!isPending && success && (
+        <span className="text-success">Added to cart...</span>
+      )}
+    </div>
+  );
+}
